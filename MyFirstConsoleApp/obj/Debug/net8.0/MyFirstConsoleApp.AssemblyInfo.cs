e42497093f56eba219aba7d3cc31bd8a0a8c5d38
HarakiryForTheSky <yegor.groznov@mail.ru>
2024-10-29 14:36:25 +0300

    Все более-менее настроил

--- MyFirstConsoleApp/obj/Debug/net8.0/MyFirstConsoleApp.AssemblyInfo.cs
+++ MyFirstConsoleApp/obj/Debug/net8.0/MyFirstConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46431728c8a82544cbc1f8e21fd5d2eda91eebec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9b9a84abcf03a6bdb2256e1120eaed32a9aa516")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
