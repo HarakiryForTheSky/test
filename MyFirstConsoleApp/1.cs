46431728c8a82544cbc1f8e21fd5d2eda91eebec
HarakiryForTheSky <yegor.groznov@mail.ru>
2024-10-29 14:17:34 +0300

    updates done by...

--- MyFirstConsoleApp/1.cs
+++ MyFirstConsoleApp/1.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyFirstConsoleApp
+    {
+    class Program
+        {
+    static void Main(string[] args)
+            {
+                while (true)
+                {
+                    Console.Write("Enter a numeric value: ");
+                    string number = Console.ReadLine();
+                    if (int.TryParse(number, out int kik))
+                    {
+                    Console.WriteLine("Successfully");
+                    break;
+                    }
+                    else
+                    {
+                    Console.WriteLine("ERROR");
+                    }
+                    Console.WriteLine("GG");
+                }
+            }
+        }
+    }
--- MySecondConsoleApp/obj/Debug/net8.0/MySecondConsoleApp.AssemblyInfo.cs
+++ MySecondConsoleApp/obj/Debug/net8.0/MySecondConsoleApp.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstConsoleApp")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("MySecondConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+241377ce86ef673ecbfcc21c67120331a6e36c40")]
-[assembly: System.Reflection.AssemblyProductAttribute("MyFirstConsoleApp")]
-[assembly: System.Reflection.AssemblyTitleAttribute("MyFirstConsoleApp")]
+[assembly: System.Reflection.AssemblyProductAttribute("MySecondConsoleApp")]
+[assembly: System.Reflection.AssemblyTitleAttribute("MySecondConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
